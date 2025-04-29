2a01b492a940f97cb4dda1407eadcc3720ebdf39
paique <bielperrett20@gmail.com>
2025-04-29 07:51:33 -0300

    First commit

---
+++ ChromeRedirect/ChromeRedirect.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Web;
+
+namespace ChromeRedirect;
+
+class ChromeRedirect
+{
+    static void Main(string[] args)
+    {
+        if (args[0] != "--single-argument")
+        {
+            Console.WriteLine(args[0]);
+            Console.WriteLine("No arguments provided.");
+            Console.ReadLine();
+            return;
+        }
+        string arguments = args[1];
+        string finalUrl = DecodeUrl(arguments);
+        
+        if (finalUrl == "")
+        {
+            Console.WriteLine("Invalid URL.");
+            Environment.Exit(1);
+        }
+        
+        RunChrome(finalUrl);
+    }
+
+    private static string DecodeUrl(string arguments)
+    {
+        Uri uri = new Uri(arguments.Replace("microsoft-edge:///?", "http://dummy/?"));
+        var queryParams = HttpUtility.ParseQueryString(uri.Query);
+        
+        string encodedUrl = queryParams["url"];
+
+        if (encodedUrl == null)
+        {
+            Console.WriteLine("Invalid URL.");
+            return "";
+        }
+
+        string firstDecode = WebUtility.UrlDecode(encodedUrl);
+        string finalUrl = WebUtility.UrlDecode(firstDecode);
+        
+        if (!Uri.IsWellFormedUriString(finalUrl, UriKind.Absolute) || !(finalUrl.StartsWith("https://") || finalUrl.StartsWith("http://")))
+        {
+            Console.WriteLine("Invalid or unsafe URL.");
+            return "";
+        }
+        
+        return finalUrl;
+    }
+
+    private static void RunChrome(string url)
+    {
+        var psi = new ProcessStartInfo
+        {
+            Arguments = url,
+            FileName = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
+        Process.Start(psi);
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Chrome Redirect
+Immediate replacement of the Microsoft Edge executable (and wipe it from existence hopefully).
+
+## 📌 Description
+This software is intended to replace the `msedge.exe` executable located at:
+
+```
+C:\Program Files (x86)\Microsoft\Edge\Application
+```
+
+Having Google Chrome installed it's a requirement:
+https://www.google.com/intl/pt-BR/chrome/
+
+Once replaced, any attempt to open Microsoft Edge (e.g., via Windows system links or `microsoft-edge://` URLs) will instead redirect to Google Chrome, using a decoded and validated URL.
+
+> 🔁 **Important:** It is strongly recommended to **backup the original `msedge.exe`** before replacing it.
+
+## ⚠️ Warning
+
+This tool was created for my **personal use**. It may contain **bugs or vulnerabilities** that have not been fully reviewed. Use it at your own risk.
